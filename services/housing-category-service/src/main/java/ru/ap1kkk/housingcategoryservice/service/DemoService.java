238d06247ecbd0652de4fb2e17e6c62c711e3cb8
Ap1kkk <apikmaster2@yandex.ru>
2024-07-21 16:12:58 +0300

    Test workflow call

--- services/housing-category-service/src/main/java/ru/ap1kkk/housingcategoryservice/service/DemoService.java
+++ services/housing-category-service/src/main/java/ru/ap1kkk/housingcategoryservice/service/DemoService.java
@@ -14,6 +14,7 @@ import java.util.Random;
 @Service
 public class DemoService {
 
+
     @Observed(name = "category.demo",
             contextualName = "getting-category-demo",
             lowCardinalityKeyValues = {"userType", "userType2"})
